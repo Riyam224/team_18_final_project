43b7c5b893391ede37a72f250e1275f1629c3a8b
Riyam Hazim <71698431+Riyam224@users.noreply.github.com>
2025-11-22 23:21:46 -0800

    Fix: remove hardcoded strings in BottomNav and use AppAssets & AppStrings constants (#8)

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,13 @@
+class AppAssets {
+  static const home = 'assets/icons/home.svg';
+  static const homeFilled = 'assets/icons/home_filled.svg';
+
+  static const market = 'assets/icons/market.svg';
+  static const marketFilled = 'assets/icons/market_filled.svg';
+
+  static const portfolio = 'assets/icons/portfolio.svg';
+  static const portfolioFilled = 'assets/icons/portfolio_filled.svg';
+
+  static const settings = 'assets/icons/settings.svg';
+  static const settingsFilled = 'assets/icons/settings_filled.svg';
+}
