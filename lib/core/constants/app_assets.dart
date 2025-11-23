a48c7844755c65d7b359e15506543be51f737ba5
Riyam <cs50riyam211@gmail.com>
2025-11-23 09:07:57 +0300

    splash feature done ui and logic and also dark and light theme applied

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,10 @@
+class AppAssets {
+  static const splashBg = 'assets/images/splash_bg.png';
+  static const splashDarkIcon = 'assets/icons/splash_dark_icon.svg';
+  static const splashLightIcon = 'assets/icons/splash_light_icon.svg';
+  static const onboarding1 = 'assets/images/onboarding_image1.png';
+  static const onboarding2 = 'assets/images/onboarding_image2.png';
+  static const onboarding3 = 'assets/images/onboarding_image3.png';
+  static const onboarding4 = 'assets/images/onboarding_image4.png';
+  static const profile = 'assets/images/profile.png';
+}
--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -28,7 +28,7 @@ class RouteGenerator {
         const Scaffold(body: Center(child: Text('404 Not Found'))),
 
     // initial route
-    initialLocation: AppRoutes.home,
+    initialLocation: AppRoutes.splash,
 
     routes: [
       // -------------------------------
