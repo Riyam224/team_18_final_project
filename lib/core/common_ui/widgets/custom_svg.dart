10ef82e4a63c4e01d59d5cdd61a60d5eefc29798
aymanshawqi360 <aymanshawqi380gmail.com>
2025-11-24 14:27:51 +0300

    - added app_svg_widget with support for asset & network SVG

---
+++ lib/core/common_ui/widgets/custom_svg.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:team_18_final_project/core/extension/app_extension.dart';
+
+class AppSvgWidget extends StatelessWidget {
+  final String? assetsName;
+  final String? networkName;
+  final BoxFit boxFit;
+  final double? height;
+  final double? width;
+  final AlignmentGeometry alignment;
+
+  const AppSvgWidget({
+    super.key,
+    this.assetsName,
+    this.networkName,
+    this.boxFit = BoxFit.contain,
+    this.height,
+    this.width,
+    this.alignment = Alignment.center,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (!assetsName.isNullOrEmp()) {
+      return SvgPicture.asset(
+        assetsName!,
+        alignment: alignment,
+        fit: boxFit,
+        height: height,
+        width: width,
+      );
+    }
+
+    return SvgPicture.network(
+      networkName ?? "",
+      alignment: alignment,
+      fit: boxFit,
+      height: height,
+      width: width,
+    );
+  }
+}
