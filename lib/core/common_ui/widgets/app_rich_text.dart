a8040d4cdbaa028097306bc6f240668d298c1a8c
aymanshawqi360 <aymanshawqi380gmail.com>
2025-11-27 01:30:29 +0300

    Implement AppRichText widget for rich clickable text

---
+++ lib/core/common_ui/widgets/app_rich_text.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:team_18_final_project/core/constants/app_spacing.dart';
+
+class AppRichText extends StatelessWidget {
+  final String fristText;
+  final String lastText;
+  final void Function()? onTap;
+  final TextStyle? fristStyle;
+  final TextStyle? lastStyle;
+  final double? horizontal;
+  const AppRichText({
+    super.key,
+    required this.fristText,
+    required this.lastText,
+    this.onTap,
+    this.fristStyle,
+    this.lastStyle,
+    this.horizontal,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        children: [
+          TextSpan(
+            recognizer: TapGestureRecognizer()..onTap = onTap,
+            style: lastStyle,
+            text: lastText,
+          ),
+          WidgetSpan(child: AppSpacing.horizontal(horizontal ?? 0)),
+          TextSpan(
+            recognizer: TapGestureRecognizer()..onTap = onTap,
+            style: fristStyle,
+            text: fristText,
+          ),
+        ],
+      ),
+    );
+  }
+}
