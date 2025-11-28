ae93a3e666da48d3e166d7e241814060ac652e9e
aymanshawqi360 <aymanshawqi380gmail.com>
2025-11-28 17:45:18 +0300

    fix the BottomActionButton problem and make more flexiblee

--- lib/core/common_ui/widgets/bottom_action_button.dart
+++ lib/core/common_ui/widgets/bottom_action_button.dart
@@ -3,7 +3,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:team_18_final_project/core/utils/app_colors.dart';
 
 class BottomActionButton extends StatelessWidget {
-  final String text;
   final VoidCallback? onPressed;
   final Color? backgroundColor;
   final Color? textColor;
@@ -15,7 +14,6 @@ class BottomActionButton extends StatelessWidget {
 
   const BottomActionButton({
     super.key,
-    required this.text,
     required this.onPressed,
     this.backgroundColor,
     this.textColor,
@@ -26,35 +24,58 @@ class BottomActionButton extends StatelessWidget {
     this.child,
   });
 
+  factory BottomActionButton.text({
+    Key? key,
+    required String text,
+    Color? textColor,
+    Color? backgroundColor,
+    double? height,
+    double? width,
+    double? fontSize,
+    BorderRadiusGeometry? borderRadiusGeometry,
+    EdgeInsetsGeometry? padding,
+    required VoidCallback? onPressed,
+  }) {
+    return BottomActionButton(
+      key: key,
+      textColor: textColor,
+      height: height,
+      width: width,
+      borderRadiusGeometry: borderRadiusGeometry,
+      padding: padding,
+      onPressed: onPressed,
+      backgroundColor: backgroundColor,
+      child: Text(
+        text,
+        style: TextStyle(
+          color: textColor,
+          fontSize: fontSize ?? 16,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: height ?? 75,
       width: width ?? double.infinity,
       child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          padding: padding ?? EdgeInsets.all(8).r,
-          backgroundColor: backgroundColor ?? AppColors.primary,
-          shape: RoundedRectangleBorder(
-            borderRadius: borderRadiusGeometry ??
-                const BorderRadius.only(
-                  topLeft: Radius.circular(0),
-                  topRight: Radius.circular(0),
-                ),
-          ),
-          elevation: 0,
-        ),
-        onPressed: onPressed,
-        child: child ??
-            Text(
-              text,
-              style: TextStyle(
-                color: textColor ?? Colors.white,
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
-              ),
+          style: ElevatedButton.styleFrom(
+            padding: padding ?? EdgeInsets.all(8).r,
+            backgroundColor: backgroundColor ?? AppColors.primary,
+            shape: RoundedRectangleBorder(
+              borderRadius: borderRadiusGeometry ??
+                  const BorderRadius.only(
+                    topLeft: Radius.circular(0),
+                    topRight: Radius.circular(0),
+                  ),
             ),
-      ),
+            elevation: 0,
+          ),
+          onPressed: onPressed,
+          child: child),
     );
   }
 }
