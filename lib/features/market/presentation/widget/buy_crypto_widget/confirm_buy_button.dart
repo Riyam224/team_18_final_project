7ecc4ee1611d3cd17bc4907c3a98766f27194824
aymanshawqi360 <aymanshawqi380gmail.com>
2025-11-28 17:58:28 +0300

    - update the ConfirmBuyButton widget

--- lib/features/market/presentation/widget/buy_crypto_widget/confirm_buy_button.dart
+++ lib/features/market/presentation/widget/buy_crypto_widget/confirm_buy_button.dart
@@ -10,20 +10,30 @@ class ConfirmBuyButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomActionButton(
-      backgroundColor:
-          context.isDark() ? AppColors.lightSurface : AppColors.primary,
-      textColor: context.isDark() ? AppColors.textDark : AppColors.textWhite,
-      height: 45.h,
-      borderRadiusGeometry: BorderRadius.circular(31),
-      text: AppStrings.buttonBuyCrypto,
-      child: Text(
-        AppStrings.buttonBuyCrypto,
-        style: context.appTheme.textTheme.headlineLarge?.copyWith(
-            color: context.isDark() ? AppColors.textDark : AppColors.textWhite,
-            fontSize: 15.sp),
-      ),
-      onPressed: () {},
-    );
+    return BottomActionButton.text(
+        backgroundColor:
+            context.isDark() ? AppColors.lightSurface : AppColors.primary,
+        textColor: context.isDark() ? AppColors.textDark : AppColors.textWhite,
+        fontSize: 15.sp,
+        borderRadiusGeometry: BorderRadius.circular(31),
+        height: 45.h,
+        text: AppStrings.buttonBuyCrypto,
+        onPressed: () {});
+
+    //  BottomActionButton(
+    //   backgroundColor:
+    //       context.isDark() ? AppColors.lightSurface : AppColors.primary,
+    //   textColor: context.isDark() ? AppColors.textDark : AppColors.textWhite,
+    //   height: 45.h,
+    //   borderRadiusGeometry: BorderRadius.circular(31),
+    //   text: AppStrings.buttonBuyCrypto,
+    //   child: Text(
+    //     AppStrings.buttonBuyCrypto,
+    //     style: context.appTheme.textTheme.headlineLarge?.copyWith(
+    //         color: context.isDark() ? AppColors.textDark : AppColors.textWhite,
+    //         fontSize: 15.sp),
+    //   ),
+    //   onPressed: () {},
+    // );
   }
 }
