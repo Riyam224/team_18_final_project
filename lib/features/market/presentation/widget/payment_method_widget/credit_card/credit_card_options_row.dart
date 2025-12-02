2e12d98deeb864efb3e179ef20e380ccd8b38852
aymanshawqi360 <aymanshawqi380gmail.com>
2025-12-02 17:03:16 +0300

    -add credit card widget components - add CreditCardWidget with gradient background support - add CreditCardOptionsRow for card type selection visa & master_card & apple_card - Implement card switching with state management integration

---
+++ lib/features/market/presentation/widget/payment_method_widget/credit_card/credit_card_options_row.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:team_18_final_project/core/common_ui/widgets/bottom_action_button.dart';
+import 'package:team_18_final_project/core/common_ui/widgets/custom_svg.dart';
+import 'package:team_18_final_project/core/config/check_credit_card_state.dart';
+import 'package:team_18_final_project/core/constants/app_spacing.dart';
+import 'package:team_18_final_project/core/constants/app_strings.dart';
+import 'package:team_18_final_project/core/extension/app_extension.dart';
+import 'package:team_18_final_project/core/utils/app_colors.dart';
+
+class CreditCardOptionsRow extends StatefulWidget {
+  final CheckCreditCardState checkCreditCardState;
+  const CreditCardOptionsRow({super.key, required this.checkCreditCardState});
+
+  @override
+  State<CreditCardOptionsRow> createState() => _CreditCardOptionsRowState();
+}
+
+class _CreditCardOptionsRowState extends State<CreditCardOptionsRow> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        AppSpacing.vertical(14),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: List.generate(AppStrings.cards.length, (int index) {
+            return BottomActionButton(
+                backgroundColor:
+                    (index == widget.checkCreditCardState.getSelectedCardIndex)
+                        ? (context.isDark()
+                            ? AppColors.primary
+                            : AppColors.darkSurface)
+                        : context.islight()
+                            ? AppColors.primary
+                            : AppColors.lightSurface,
+                borderRadiusGeometry: BorderRadius.circular(12).r,
+                onPressed: () {
+                  setState(() {
+                    widget.checkCreditCardState.selectCardByIndex(index: index);
+                  });
+                },
+                height: 36.h,
+                width: 93.w,
+                child: AppSvgWidget(
+                  color: (index == 1)
+                      ? null
+                      : (index ==
+                              widget.checkCreditCardState.getSelectedCardIndex)
+                          ? (context.isDark()
+                              ? AppColors.textWhite
+                              : AppColors.textWhite)
+                          : context.islight()
+                              ? AppColors.textWhite
+                              : AppColors.textDark,
+                  assetsName: AppStrings.cards[index],
+                ));
+          }),
+        ),
+        Visibility(
+            maintainSize: true,
+            maintainAnimation: true,
+            maintainState: true,
+            maintainSemantics: true,
+            maintainInteractivity: true,
+            visible: widget.checkCreditCardState.getIsCreditCardVisible,
+            child: widget.checkCreditCardState.getCardType
+                .buildCardWidget(context: context))
+      ],
+    );
+  }
+}
