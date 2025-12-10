a05173a1956775f17cde6641937c01610207dfc3
roma5555 <romamohamed728@gmail.com>
2025-12-10 23:16:48 +0200

    fix: Reapplying final changes and preparing for merge sync

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -42,7 +42,7 @@ class RouteGenerator {
     observers: [appRouteObserver],
     errorBuilder: (context, state) =>
         Scaffold(body: Center(child: Text(AppStrings.notFound))),
-    initialLocation: AppRoutes.payment,
+    initialLocation: AppRoutes.splash,
 
     // ==============================
     // 🔐 REDIRECT FIXED HERE
