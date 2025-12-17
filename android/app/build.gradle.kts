28b4cb09e3a3738d9eee3878cbafba5d70e51a05
Riyam <cs50riyam211@gmail.com>
2025-12-17 12:15:04 +0300

    flavors for android done - dev and prod

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,6 +8,41 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+// android {
+//     namespace = "com.example.team_18_final_project"
+//     compileSdk = flutter.compileSdkVersion
+//     ndkVersion = flutter.ndkVersion
+
+//     compileOptions {
+//         sourceCompatibility = JavaVersion.VERSION_17
+//         targetCompatibility = JavaVersion.VERSION_17
+//     }
+
+//     kotlinOptions {
+//         jvmTarget = JavaVersion.VERSION_17.toString()
+//     }
+
+//     defaultConfig {
+//         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+//         applicationId = "com.example.team_18_final_project"
+//         // You can update the following values to match your application needs.
+//         // For more information, see: https://flutter.dev/to/review-gradle-config.
+//         minSdk = flutter.minSdkVersion
+//         targetSdk = flutter.targetSdkVersion
+//         versionCode = flutter.versionCode
+//         versionName = flutter.versionName
+//     }
+
+//     buildTypes {
+//         release {
+//             // TODO: Add your own signing config for the release build.
+//             // Signing with the debug keys for now, so `flutter run --release` works.
+//             signingConfig = signingConfigs.getByName("debug")
+//         }
+//     }
+// }
+
+// todo add flavors env to android 
 android {
     namespace = "com.example.team_18_final_project"
     compileSdk = flutter.compileSdkVersion
@@ -23,20 +58,31 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.team_18_final_project"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    // 🔥 ADD THIS
+    flavorDimensions += "env"
+
+    productFlavors {
+        create("dev") {
+            dimension = "env"
+            applicationIdSuffix = ".dev"
+            resValue("string", "app_name", "FinTech Dev")
+        }
+
+        create("prod") {
+            dimension = "env"
+            resValue("string", "app_name", "FinTech")
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
